168b2115b7826c61496a5e5b5b9f1ff2310289ee
Marcos Mendonça <marcos.mendonca@matera.com>
2021-04-13 11:50:49 -0300

    6.9 Adicionando CORS no método para permitir requisição de uma origem especifica (Poderia adicionar o CORS para toda a controladora, permitindo todas as requisições de Categoria

--- src/main/java/com/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algamoney/api/resource/CategoriaResource.java
@@ -24,6 +24,7 @@ public class CategoriaResource {
     @Autowired
     private ApplicationEventPublisher publisher;
 
+    @CrossOrigin(maxAge = 10, origins = { "http://localhost:8000" })
     @GetMapping
     public List<Categoria> listar() {
          return categoriaRepository.findAll();
