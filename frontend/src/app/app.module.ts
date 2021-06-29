a5ed47756ac2e28e4aa731ba6e79a93e70b94d50
Marcos <marcos.mendonca@matera.com>
2021-06-28 21:19:31 -0300

    17.11. Alterando o locale da aplicação para pt-BR -> Corrige formatação de valores monetários e etc...

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { AppRoutingModule } from './app-routing.module';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -7,12 +7,17 @@ import {ToastModule} from 'primeng/toast';
 import {ConfirmDialogModule} from 'primeng/confirmdialog';
 import {ConfirmationService} from 'primeng/api';
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
 import { CoreModule } from './core/core.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentoService } from './lancamentos/lancamento.service';
 import { PessoaService } from './pessoas/pessoa.service';
 import { MessageService } from 'primeng/api';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -29,6 +34,8 @@ import { MessageService } from 'primeng/api';
     ConfirmDialogModule
   ],
   providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+
     MessageService,
     ConfirmationService,
     LancamentoService,
