c18ee9bb38ff54f7cf3c1cad46919352dde5cc4b
Marcos <marcos.mendonca@matera.com>
2021-07-09 21:41:48 -0300

    Fix: comentário

--- frontend/server.js
+++ frontend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 // diretório dist é onde está o código compilado que será executado
+// podemos alterar o diretório que será gerada a build -> angular.json / outputPath
 app.use(express.static(__dirname + '/dist/frontend'));
 
 // qualquer rota vai servir o index.html
