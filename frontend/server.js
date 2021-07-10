d96da774896b2820bf5ffe4c6246c772be9d85b0
Marcos <marcos.mendonca@matera.com>
2021-07-09 21:34:50 -0300

    20.3. Respondendo requisições com Node.js e o framework Express

---
+++ frontend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// diretório dist é onde está o código compilado que será executado
+app.use(express.static(__dirname + '/dist/frontend'));
+
+// qualquer rota vai servir o index.html
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + '/dist/frontend/index.html');
+});
+
+app.listen(4200); // porta
+
+// Para rodar, execute no terminal o comando:
+// node server.js
